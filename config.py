ad761eed8d2992cbf388e3f7ba47a43d087e0968
Lauri Junkkari <latenssi@gmail.com>
2021-11-29 16:41:45 +0200

    Update

--- config.py
+++ config.py
@@ -2,9 +2,10 @@
 
 import os
 
+
 class BaseConfig(object):
-    SECRET_KEY = os.environ['SECRET_KEY'] 
+    SECRET_KEY = os.environ['SECRET_KEY']
     DEBUG = os.environ['DEBUG']
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', '/tmp')
     ALLOWED_EXTENSIONS = set(['csv'])
-    MAX_CONTENT_LENGTH = 512 * 1024 # 512 KB
+    MAX_CONTENT_LENGTH = 512 * 1024  # 512 KB
--- server.py
+++ server.py
@@ -7,11 +7,14 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
 
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in BaseConfig.ALLOWED_EXTENSIONS
 
+
 def change_ending(filename, ending):
     return "{0}.{1}".format(os.path.splitext(filename)[0], ending)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -31,13 +34,16 @@ def index():
             file.save(filepath)
 
             ofx_filename = change_ending(filename, 'ofx')
-            ofx_filepath = os.path.join(app.config['UPLOAD_FOLDER'], ofx_filename)
+            ofx_filepath = os.path.join(
+                app.config['UPLOAD_FOLDER'], ofx_filename)
 
-            subprocess.call(["ofxstatement", "convert", "-t", "op:eur", filepath, ofx_filepath])
+            subprocess.call(["ofxstatement", "convert", "-t",
+                            "op", filepath, ofx_filepath])
 
             return send_file(ofx_filepath, as_attachment=True)
 
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
